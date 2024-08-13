25bc66d6f086fd8f3cf3605cceceb06c7e45f5d3
wonkyo <dnjsry159@naver.com>
2024-08-13 22:10:56 +0800

    캘린더 작업 중

--- lib/screens/account/introscreen.dart
+++ lib/screens/account/introscreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:todomate/screens/account/loginscreen.dart';
+import 'package:todomate/screens/diary/diary.dart';
 
 class IntroScreen extends StatefulWidget {
   const IntroScreen({super.key});
@@ -41,7 +42,7 @@ class _IntroScreenState extends State<IntroScreen> {
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
+        MaterialPageRoute(builder: (context) => const DiaryCalendarScreen()),
       );
     });
   }
--- lib/screens/diary/diary.dart
+++ lib/screens/diary/diary.dart
@@ -1,1 +1,57 @@
-//class diary{}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class DiaryCalendarScreen extends StatelessWidget {
+  const DiaryCalendarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: TableCalendar(
+            locale: 'ko-KR',
+            focusedDay: DateTime.now(),
+            firstDay: DateTime.now().subtract(const Duration(days: 60)),
+            lastDay: DateTime.now().add(const Duration(days: 60)),
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+              titleTextStyle: const TextStyle(
+                color: Colors.black,
+              ),
+              headerPadding: const EdgeInsets.symmetric(vertical: 4.0),
+              leftChevronIcon: const SizedBox.shrink(), // 아이콘 숨기기
+              rightChevronIcon: const SizedBox.shrink(), // 아이콘 숨기기
+            ),
+            calendarBuilders: CalendarBuilders(
+              headerTitleBuilder: (context, day) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      DateFormat.yMMMM('ko-KR').format(day),
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    Row(
+                      children: const [
+                        Icon(Icons.arrow_left),
+                        SizedBox(width: 8.0),
+                        Icon(Icons.arrow_right),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
