d1194742c15e8ab5c03cf276dd7d2f9cf644acc5
wonkyo <dnjsry159@naver.com>
2024-08-20 18:21:23 +0900

    Sharedpreference 추가 및 login -> chat_screen (ProfileScreen 이동시 에만)연동

--- lib/util/sharedpreference.dart
+++ lib/util/sharedpreference.dart
@@ -1,17 +1,42 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-class TodoSharedPreference{
-  static final TodoSharedPreference _instance = TodoSharedPreference._internal();
-  factory TodoSharedPreference() => _instance;
 
+class TodoSharedPreference{
+  static TodoSharedPreference? _instance;
   static SharedPreferences? _prefs;
 
-  TodoSharedPreference._internal()
+  TodoSharedPreference._internal();
 
-  Future<SharedPreferences?> get prefs async{
-    if(_prefs != null) return _prefs;
+  factory TodoSharedPreference() => _instance ??= TodoSharedPreference._internal();
 
+  Future<SharedPreferences> get prefs async {
+    if(_prefs != null) return _prefs!;
     _prefs = await SharedPreferences.getInstance();
-    return _prefs;
+    return _prefs!;
+  }
+
+  // 데이터 저장
+  Future<void> setPreferenceWithKey(String key, String value) async {
+    SharedPreferences preferences = await prefs;
+    preferences.setString(key, value);
+  }
+
+  // 데이터 읽기
+  Future<String> getPreferenceWithKey(String key) async {
+    SharedPreferences preferences = await prefs;
+    return preferences.getString(key) ?? "";
+  }
+
+  // 데이터 삭제
+  Future<void> removePreferenceWithKey(String key) async {
+    SharedPreferences preferences = await prefs;
+    await preferences.remove(key);
+  }
+
+  // 전체 데이터 삭제
+  Future<void> clearPreference() async {
+    SharedPreferences preferences = await prefs;
+    await preferences.clear();
   }
 }
+
