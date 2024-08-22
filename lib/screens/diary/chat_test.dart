1fb5ae732eaf6182a29114064375c026f1e07222
[APP1] 국한 <kook1488@naver.com>
2024-08-22 17:49:46 +0900

    알림기능은 넣었는데, 소켓연결이 안되서 확인이 안되는 상황

--- lib/screens/diary/chat_test.dart
+++ lib/screens/diary/chat_test.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart';
-import 'package:todomate/chat/core/scroll_controller_mixin.dart';
+
+//노드 제이에스- 서버  원교님 쓴느 와이파이로 옴
+//원교님 동네로 간것
+//json 으로.
 
 class ChatScreenTest extends StatefulWidget {
   final String roomId;
@@ -10,7 +13,8 @@ class ChatScreenTest extends StatefulWidget {
   State<ChatScreenTest> createState() => _ChatScreenTestState();
 }
 
-class _ChatScreenTestState extends State<ChatScreenTest> with ScrollControlMixin{
+class _ChatScreenTestState extends State<ChatScreenTest>
+    with ScrollControlMixin {
   late Socket socket; // 별칭 없이 직접 사용
   late TextEditingController _messageController;
   //채팅방 메시지 객체 리스트
@@ -27,14 +31,14 @@ class _ChatScreenTestState extends State<ChatScreenTest> with ScrollControlMixin
 
   void _connectSocket() {
     //IP등록
-    socket = io('https://9cb9-59-6-82-123.ngrok-free.app', <String, dynamic>{
+    socket = io('https://be35-59-6-82-123.ngrok-free.app', <String, dynamic>{
       'transports': ['websocket'],
     });
 
     socket.on('connect', (_) {
       print('Connected to server');
       //roomId로 룸 가입
-      socket.emit('joinRoom',widget.roomId);
+      socket.emit('joinRoom', widget.roomId);
     });
 
     //연결 성공시 서버에서 socketId 보내주면 초기화
@@ -44,7 +48,6 @@ class _ChatScreenTestState extends State<ChatScreenTest> with ScrollControlMixin
         _socketId = socketId;
       });
     });
-
 
     socket.on('receiveMessage', (message) {
       //채팅 객체를 josn형태로 받아서 ChatDTO로 파싱
@@ -65,13 +68,13 @@ class _ChatScreenTestState extends State<ChatScreenTest> with ScrollControlMixin
       //emit할때 배열로 인자 2개(message,roomId) 안 넣으면 receiveMessage가 안옴 ->
       // 룸안에 있는 사람에게 전역으로 revicemessage를 주기 때문
 
-      ChatDTO sendChatDTO = ChatDTO(id: _socketId, message: _messageController.text );
+      ChatDTO sendChatDTO =
+          ChatDTO(id: _socketId, message: _messageController.text);
       //채팅 객체를 json으로 파싱해서 보냄
       socket.emit('sendMessage', [sendChatDTO.toJson(), widget.roomId]);
       _messageController.clear();
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -97,7 +100,7 @@ class _ChatScreenTestState extends State<ChatScreenTest> with ScrollControlMixin
                   horizontal: 14,
                 ),
                 separatorBuilder: (context, index) =>
-                const SizedBox(height: 10),
+                    const SizedBox(height: 10),
                 itemCount: chats.length,
                 itemBuilder: (context, index) {
                   //현재 내 socketId와 같으면 오른쪽 메시지 다르면 왼쪽 메시지
@@ -111,7 +114,8 @@ class _ChatScreenTestState extends State<ChatScreenTest> with ScrollControlMixin
                       Container(
                         //가로로 채팅 최대로 넘어갈 시 오류나서 최대값 정해줌
                         constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.7, // 말풍선의 최대 너비 설정
+                          maxWidth: MediaQuery.of(context).size.width *
+                              0.7, // 말풍선의 최대 너비 설정
                         ),
                         padding: const EdgeInsets.all(14),
                         decoration: BoxDecoration(
@@ -170,7 +174,7 @@ class _ChatScreenTestState extends State<ChatScreenTest> with ScrollControlMixin
                   ),
                   const SizedBox(width: 10),
                   ElevatedButton(
-                    onPressed: () =>{
+                    onPressed: () => {
                       _sendMessage(),
                       //전송버튼 누르면 보내던것 초기화
                       _messageController.clear()
@@ -187,7 +191,7 @@ class _ChatScreenTestState extends State<ChatScreenTest> with ScrollControlMixin
   }
 }
 
-class ChatDTO{
+class ChatDTO {
   String id;
   String message;
 
