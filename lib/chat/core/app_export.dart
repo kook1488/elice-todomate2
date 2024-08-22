f8629fc72c4819a73012a6976bcbd50a81929672
wonkyo <dnjsry159@naver.com>
2024-08-22 15:43:08 +0900

    untrack file commi, 추ê가commit

--- lib/chat/core/app_export.dart
+++
@@ -1,3 +0,0 @@
-export 'package:todomate/chat/core/utils/color_constant.dart';
-export 'package:todomate/chat/core/utils/image_constant.dart';
-export 'package:todomate/chat/core/utils/math_utils.dart';
--- lib/util/string_utils.dart
+++
@@ -1,4 +0,0 @@
-import 'package:intl/intl.dart';
-
-// ####.#.# 형식 Format
-String dateToString(DateTime day) => DateFormat('yyyy.MM.dd').format(day);
